5812eca8d74b6641368040e8f8d009cbbffb8314
Andrew McMillan <andrew@lamb.(none)>
2005-08-29 23:35:36 +1200

    An initial workover to remove global variables and tidy the code somewhat.

--- inc/Session.php
+++ inc/Session.php
@@ -2,51 +2,45 @@
 // Session handling
 // - set up the session object
 
-include_once('PgQuery.php');
+require_once('PgQuery.php');
 
-if ( isset($logout) || (isset($M) && $M=='LO') )
-{
+if ( isset($_GET['logout']) ) {
   error_log("$sysname: Session: DBG: Logging out");
   setcookie( 'sid', '', 0,'/');
-  unset($sid);
+  unset($_COOKIE['sid']);
+  unset($_COOKIE['lsid']); // Allow a cookied person to be un-logged-in for one page view.
 
-  if ( isset($forget) ) {
-    setcookie( 'lsid', '', 0,'/');
-    unset($lsid);
-  }
+  if ( isset($_GET['forget']) ) setcookie( 'lsid', '', 0,'/');
 }
 
+$session = new Session();
 
-if ( !isset($session) ) {
-  $session = new Session();
-
-  if ( isset($username) && isset($password) ) {
-    // Try and log in if we have a username and password
-    $session->Login( $username, $password );
-    if ( $debuggroups['Login'] )
-      $session->Log( "DBG: User $username - $session->fullname ($session->user_no) login status is $session->logged_in" );
-  }
-  else if ( !isset($sid) && isset($lsid) && $lsid != "" ) {
-    // Validate long-term session details
-    $session->LSIDLogin( $lsid );
-    if ( $debuggroups['Login'] )
-      $session->Log( "DBG: User $username - $session->fullname ($session->user_no) login status is $session->logged_in" );
-  }
+if ( isset($_POST['username']) && isset($_POST['password']) ) {
+  // Try and log in if we have a username and password
+  $session->Login( $_POST['username'], $_POST['password'] );
+  if ( $debuggroups['Login'] )
+    $session->Log( "DBG: User $_POST[username] - $session->fullname ($session->user_no) login status is $session->logged_in" );
+}
+else if ( !isset($_COOKIE['sid']) && isset($_COOKIE['lsid']) && $_COOKIE['lsid'] != "" ) {
+  // Validate long-term session details
+  $session->LSIDLogin( $_COOKIE['lsid'] );
+  if ( $debuggroups['Login'] )
+    $session->Log( "DBG: User $session->username - $session->fullname ($session->user_no) login status is $session->logged_in" );
 }
 
 function session_salted_md5( $instr, $salt = "" ) {
-  global $sysabbr, $debuggroups;
+  global $debuggroups, $session;
   if ( $salt == "" ) $salt = substr( md5(rand(100000,999999)), 2, 8);
   if ( $debuggroups['Login'] )
-    error_log( "$sysabbr: DBG: Making salted MD5: salt=$salt, instr=$instr, md5($salt$instr)=".md5($salt . $instr) );
+    $session->Log( "DBG: Making salted MD5: salt=$salt, instr=$instr, md5($salt$instr)=".md5($salt . $instr) );
   return ( sprintf("*%s*%s", $salt, md5($salt . $instr) ) );
 }
 
 function session_validate_password( $they_sent, $we_have ) {
-  global $system_name, $debuggroups;
+  global $debuggroups, $session;
 
   if ( $debuggroups['Login'] )
-    error_log( "$system_name: Session: DBG: Comparing they_sent=$they_sent with $we_have" );
+    $session->Log( "DBG: Comparing they_sent=$they_sent with $we_have" );
 
   // In some cases they send us a salted md5 of the password, rather
   // than the password itself (i.e. if it is in a cookie)
@@ -67,7 +61,7 @@ function session_validate_password( $they_sent, $we_have ) {
     $salt = $regs[1];
     $md5_sent = session_salted_md5( $they_sent, $salt ) ;
     if ( $debuggroups['Login'] )
-      error_log( "$system_name: Session-vpw: DBG: Salt=$salt, comparing=$md5_sent with $pwcompare or $we_have" );
+      $session->Log( "DBG: Salt=$salt, comparing=$md5_sent with $pwcompare or $we_have" );
     return ( $md5_sent == $pwcompare );
   }
 
@@ -93,34 +87,21 @@ class Session
 
   function Session()
   {
-    global $sid, $sysname;
-
     $this->roles = array();
     $this->logged_in = false;
     $this->just_logged_in = false;
 
-    if ( ! isset($sid) ) return;
+    if ( ! isset($_COOKIE['sid']) ) return;
 
-    list( $session_id, $session_key ) = explode( ';', $sid, 2 );
+    list( $session_id, $session_key ) = explode( ';', $_COOKIE['sid'], 2 );
 
-    if ( $GLOBALS['pg_version'] == 7.2 ) {
-      $sql = "SELECT session.*, usr.*, organisation.*
-        FROM session, usr, organisation
-        WHERE usr.user_no = session.user_no
-        AND session_id = ?
-        AND (session_key = ? OR session_key = ?)
-        AND organisation.org_code = usr.org_code
-        ORDER BY session_start DESC LIMIT 1";
-    }
-    else {
-      $sql = "SELECT session.*, usr.*, organisation.*
+    $sql = "SELECT session.*, usr.*, organisation.*
         FROM session, usr, organisation
         WHERE usr.user_no = session.user_no
         AND session_id = ?
         AND (md5(session_start::text) = ? OR session_key = ?)
         AND organisation.org_code = usr.org_code
         ORDER BY session_start DESC LIMIT 1";
-    }
 
     $qry = new PgQuery($sql, $session_id, $session_key, $session_key);
     if ( $qry->Exec('Session') && $qry->rows == 1 )
@@ -140,74 +121,64 @@ class Session
 
   function Log( $whatever )
   {
-    global $sysabbr;
+    global $c;
 
     $argc = func_num_args();
     $format = func_get_arg(0);
     if ( $argc == 1 || ($argc == 2 && func_get_arg(1) == "0" ) ) {
-      error_log( "$sysabbr: $format" );
+      error_log( "$c->sysabbr: $format" );
     }
     else {
       $args = array();
       for( $i=1; $i < $argc; $i++ ) {
         $args[] = func_get_arg($i);
       }
-      error_log( "$sysabbr: " . vsprintf($format,$args) );
+      error_log( "$c->sysabbr: " . vsprintf($format,$args) );
     }
     return true;
   }
 
-  function AllowedTo ( $whatever )
-  {
+  function AllowedTo ( $whatever ) {
     return ( $this->logged_in && isset($this->roles[$whatever]) && $this->roles[$whatever] );
   }
 
 
-  function GetRoles ()
-  {
+  function GetRoles () {
     $this->roles = array();
     $qry = new PgQuery( 'SELECT group_name AS role_name FROM group_member m join ugroup g ON g.group_no = m.group_no WHERE user_no = ? ', $this->user_no );
-    if ( $qry->Exec('Session::GetRoles') && $qry->rows > 0 )
-    {
-      while( $role = $qry->Fetch() )
-      {
+    if ( $qry->Exec('Session::GetRoles') && $qry->rows > 0 ) {
+      while( $role = $qry->Fetch() ) {
         $this->roles[$role->role_name] = true;
       }
     }
 
     $this->system_roles = array();
     $qry = new PgQuery( 'SELECT system_code, role FROM system_usr WHERE user_no = ? ', $this->user_no );
-    if ( $qry->Exec('Session::GetRoles') && $qry->rows > 0 )
-    {
-      while( $role = $qry->Fetch() )
-      {
+    if ( $qry->Exec('Session::GetRoles') && $qry->rows > 0 ) {
+      while( $role = $qry->Fetch() ) {
         $this->system_roles[$role->system_code] = $role->role;
       }
     }
   }
 
 
-  function AssignSessionDetails( $u )
-  {
+  function AssignSessionDetails( $u ) {
     $this->user_no = $u->user_no;
     $this->username = $u->username;
     $this->fullname = $u->fullname;
     $this->email = $u->email;
     $this->org_code = $u->org_code;
     $this->org_name = $u->org_name;
-    $this->base_rate = $u->base_rate;
-    $this->work_rate = $u->work_rate;
     $this->config_data = $u->config_data;
     $this->session_id = $u->session_id;
 
-    // $this->roles = explode( "|", $session_stuff->roles );
     $this->GetRoles();
     $this->logged_in = true;
   }
 
 
   function Login( $username, $password ) {
-    global $sysname, $sid, $debuggroups, $client_messages, $remember;
+    global $c, $debuggroups;
     if ( $debuggroups['Login'] )
       $this->Log( "DBG: Login: Attempting login for $username" );
 
@@ -237,12 +208,14 @@ class Session
             // Recognise that we have started a session now too...
             $this->Session();
             $this->Log( "DBG: Login: INFO: New session $session_id started for $username ($usr->user_no)" );
-            if ( isset($remember) && intval($remember) > 0 ) {
+            if ( isset($_POST['remember']) && intval($_POST['remember']) > 0 ) {
               $cookie .= md5( $this->user_no ) . ";";
               $cookie .= session_salted_md5($usr->user_no . $usr->username . $usr->password);
-              setcookie( "lsid", $cookie, time() + (86400 * 3600), "$base_url/" );   // will expire in ten or so years
+              setcookie( "lsid", $cookie, time() + (86400 * 3600), "/" );   // will expire in ten or so years
             }
             $this->just_logged_in = true;
+            unset($_POST['submit']);
+            unset($_GET['submit']);
             unset($GLOBALS['submit']);
             return true;
           }
@@ -340,21 +313,21 @@ class Session
 
 
   function LoginRequired( $groups = "" ) {
-    global $system_name, $admin_email, $session, $images, $colors;
+    global $c, $session;
 
     if ( $this->logged_in && $groups == "" ) return;
     if ( ! $this->logged_in ) {
-      $client_messages[] = "You must log in to use this system.";
-      include("headers.php");
+      $c->messages[] = "You must log in to use this system.";
+      include("page-header.php");
       if ( function_exists("local_index_not_logged_in") ) {
         local_index_not_logged_in();
       }
       else {
         echo <<<EOTEXT
-<H4>For access to the $system_name you should log on with
+<H4>For access to the $c->system_name you should log on with
 the username and password that have been issued to you.</H4>
 
-<h4>If you would like to request access, please e-mail $admin_email.</h4>
+<h4>If you would like to request access, please e-mail $c->admin_email.</h4>
 EOTEXT;
       }
     }
@@ -363,11 +336,11 @@ EOTEXT;
       foreach( $valid_groups AS $k => $v ) {
         if ( $this->AllowedTo($v) ) return;
       }
-      include("headers.php");
-      $client_messages[] = "You are not authorised to use this function.";
+      include("page-header.php");
+      $c->messages[] = "You are not authorised to use this function.";
     }
 
-    include("footers.php");
+    include("page-footer.php");
     exit;
   }
 }

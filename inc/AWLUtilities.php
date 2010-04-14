1c27bb6fdd97cf423179c157d08b948e8741125a
Andrew McMillan <andrew@morphoss.com>
2010-04-14 15:45:12 +1200

    Fix strange alternate reality definition of aparche_read_header.

--- inc/AWLUtilities.php
+++ inc/AWLUtilities.php
@@ -60,19 +60,17 @@ if ( !function_exists('apache_request_headers') ) {
   * Compatibility so we can use the apache function name and still work with CGI
   * @package awl
   */
-  function apache_request_headers() {
-    eval('
-        function apache_request_headers() {
-            foreach($_SERVER as $key=>$value) {
-                if (substr($key,0,5)=="HTTP_") {
-                    $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
-                    $out[$key]=$value;
-                }
+  eval('
+    function apache_request_headers() {
+        foreach($_SERVER as $key=>$value) {
+            if (substr($key,0,5)=="HTTP_") {
+                $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
+                $out[$key]=$value;
             }
-            return $out;
         }
-    ');
-  }
+        return $out;
+    }
+  ');
 }
 
 

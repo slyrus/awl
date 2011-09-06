5d7b376d0daa9ff4dfa5af7b87fdcb2367395a7b
Andrew McMillan <andrew@morphoss.com>
2011-09-06 23:43:05 +1200

    [AwlQuery] Change phpdoc to include possible values for TransactionState()

--- inc/AwlQuery.php
+++ inc/AwlQuery.php
@@ -449,6 +449,7 @@ class AwlQuery
   /**
   * Returns the current state of a transaction, indicating if we have begun a transaction, whether the transaction
   * has failed, or if we are not in a transaction.
+  * @return int 0 = not started, 1 = in progress, -1 = error pending rollback/commit
   */
   function TransactionState() {
     global $_awl_dbconn;

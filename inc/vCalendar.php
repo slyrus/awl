c327cf56b2981f827773ccfd2408c002da9d90f9
Andrew McMillan <andrew@morphoss.com>
2011-09-08 16:00:25 +1200

    [vCalendar] Developing this to be useful for scheduling.

--- inc/vCalendar.php
+++ inc/vCalendar.php
@@ -22,9 +22,19 @@ require_once('vComponent.php');
 
 class VCalendar extends vComponent {
 
-  var $contained_type;
-  var $timezones;
-
+  /**
+   * These variables are mostly used to improve efficiency by caching values as they are
+   * retrieved to speed any subsequent access.
+   * @var string $contained_type
+   * @var array $timezones
+   * @var string $organizer
+   * @var array $attendees
+   */
+  private $contained_type;
+  private $timezones;
+  private $organizer;
+  private $attendees;
+  
   /**
    * Constructor.  If a string is passed it will be parsed as if it was an iCalendar object,
    * otherwise a new vCalendar will be initialised with basic content. If an array of key value
@@ -84,6 +94,38 @@ class VCalendar extends vComponent {
   function GetTimeZone( $tzid ) {
     if ( empty($this->timezones[$tzid]) ) return null;
     return $this->timezones[$tzid];
+  }
+
+
+  /**
+   * Get the organizer of this VEVENT/VTODO
+   */
+  function GetOrganizer() {
+    if ( !isset($this->organizer) ) {
+      $organizers = $this->GetPropertiesByPath('/VCALENDAR/*/ORGANIZER');
+      $organizer = (count($organizers) > 0 ? $organizers[0]->Value() : false);
+      $this->organizer = (empty($organizer) ? false : $organizer );
+    }
+    return $this->organizer;
+  }
+
+  
+  /**
+   * Get the attendees of this VEVENT/VTODO
+   */
+  function GetAttendees() {
+    if ( !isset($this->attendees) ) {
+      $attendees = $this->GetPropertiesByPath('/VCALENDAR/*/ATTENDEE');
+      $wr_attendees = $this->GetPropertiesByPath('/VCALENDAR/*/X-WR-ATTENDEE');
+      if ( count ( $wr_attendees ) > 0 ) {
+        dbg_error_log( 'PUT', 'Non-compliant iCal request.  Using X-WR-ATTENDEE property' );
+        foreach( $wr_attendees AS $k => $v ) {
+          $attendees[] = $v;
+        }
+      }
+      $this->attendees = $attendees;
+    }
+    return $this->attendees;
   }
 
 }
